ccfc988bad518279fcdb282380cd25328558259c
mekdeslake <133536842+mekdeslake@users.noreply.github.com>
2023-06-13 12:46:55 -0700

    Update drinksController.java

--- JavaProjects-part2/java/com/example/superadminportal/drinksController.java
+++ JavaProjects-part2/java/com/example/superadminportal/drinksController.java
@@ -1,14 +1,26 @@
-<?xml version="1.0" encoding="UTF-8"?>
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 
-<?import java.lang.*?>
-<?import java.util.*?>
-<?import javafx.scene.*?>
-<?import javafx.scene.control.*?>
-<?import javafx.scene.layout.*?>
+public class drinksController {
 
-<AnchorPane xmlns="http://javafx.com/javafx"
-            xmlns:fx="http://javafx.com/fxml"
-            fx:controller="com.example.superadmin.portal.JavaProjects-part2.JavaProjects-part2.java.com.example.superadminportal.DrinksControllerJava"
-            prefHeight="400.0" prefWidth="600.0">
+    @FXML
+    private MenuItem about;
 
-</AnchorPane>
+    @FXML
+    private MenuItem drinks;
+
+    @FXML
+    private MenuItem foods;
+
+    @FXML
+    private MenuItem login;
+
+    @FXML
+    private MenuButton option;
+
+    @FXML
+    private MenuItem signup;
+
+}
+
